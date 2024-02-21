54fe035b93a06ef67fb4aa83211e9fbfa63d0e65
asmimore <35525461+asmimore@users.noreply.github.com>
2024-02-21 18:11:31 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "ap-south-1"  
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-0e670eb768a5fc3d4" 
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance"
+  }
+}
